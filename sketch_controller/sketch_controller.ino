8f4032143fa160c0d9909110989e65192828e084
artificial256 <artificial256@gmail.com>
2016-12-07 16:08:48 -0700

    Add change
    Changed to interact with keyboard directional pad and also keyboard
    "letters"

--- sketch_controller/sketch_controller.ino
+++ sketch_controller/sketch_controller.ino
@@ -7,6 +7,8 @@ This skectch is using pull up resistors.
 
 */
 
+#include <Keyboard.h>
+
 int UD = 0;
 int LR = 0;
 /* Arduino Micro output pins*/
@@ -21,7 +23,16 @@ int ILR=A1;
 int MID = 9; // 10 mid point delta arduino, use 4 for attiny
 int LRMID = 0;
 int UPMID = 0;
+
+const int button1Pin = 3;  // pushbutton 1 pin
+const int button2Pin = 4;
+int button1State, button2State;
+
 void setup(){
+  pinMode(button1Pin, INPUT);
+  pinMode(button2Pin, INPUT);
+  
+
   
   pinMode(DWN, OUTPUT);
   pinMode(UP, OUTPUT);  
@@ -39,36 +50,64 @@ void setup(){
 }
 
 void loop(){
+  button1State = digitalRead(button1Pin);
+  button2State = digitalRead(button2Pin);
+
+  Serial.println(button1State); 
+  Serial.println(button2State); 
+  
+
+  if (button1State){
+    Keyboard.press('x');
+  }
+  else {
+    Keyboard.release('x');
+  }
+    if (button2State){
+    Keyboard.press('c');
+  }
+  else {
+    Keyboard.release('c');
+  }
 
   UD = analogRead(IUP);
   LR = analogRead(ILR);
+  
   // UP-DOWN
   if(UD < UPMID - MID){
-   digitalWrite(DWN, LOW);
+    digitalWrite(DWN, LOW);
+    Keyboard.press(KEY_DOWN_ARROW);
   }else{
-   digitalWrite(DWN, HIGH);
+    digitalWrite(DWN, HIGH);
+    Keyboard.release(KEY_DOWN_ARROW);
   }
   
-  if(UD > UPMID + MID){
-   digitalWrite(UP, LOW);
+   if(UD > UPMID + MID){
+    digitalWrite(UP, LOW);
+    Keyboard.press(KEY_UP_ARROW);
   }else{
    digitalWrite(UP, HIGH);
+   Keyboard.release(KEY_UP_ARROW); 
   }
+  
   // LEFT-RIGHT
   if(LR < LRMID - MID){
-   digitalWrite(LEFT, LOW);
+    digitalWrite(LEFT, LOW);
+    Keyboard.press(KEY_LEFT_ARROW);
   }else{
-   digitalWrite(LEFT, HIGH);
+     digitalWrite(LEFT, HIGH);
+    Keyboard.release(KEY_LEFT_ARROW);
   }
   
   if(LR > LRMID + MID){
    digitalWrite(RT, LOW);
+   Keyboard.press(KEY_RIGHT_ARROW);
   }else{
    digitalWrite(RT, HIGH);
+   Keyboard.release(KEY_RIGHT_ARROW);
   }
  
-  delay(400);
-
+  delay(0);
 
 }
 
